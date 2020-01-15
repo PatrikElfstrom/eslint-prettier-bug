654b2c48e13bf38d20e77b21144f4b3f88353a1d
Patrik Elfström <patrik.elfstrom@humblebee.se>
2020-01-15 10:52:33 +0100

    First commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+
+export default class MyClass {
+    myMethod() {
+        return {
+                callbacks: {
+                    label: () => {
+                        if (true) {
+                            if (true) {
+                                return '';
+                            }
+                            else if (true) {
+                                return '';
+                            }
+                        }
+                    }
+                }
+            };
+    }
+}
